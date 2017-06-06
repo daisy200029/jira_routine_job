60e8f5ae8abdb0d5b75ef2ac7def09f025c1d44a
dliu <dliu@changehealthcare.com>
2017-06-06 15:28:22 +0800

    handle authentication exception

--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, TextAreaField, SubmitField, validators, ValidationError, SelectField
+from wtforms import TextField, TextAreaField, SubmitField, validators, ValidationError, SelectField, PasswordField
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 class ContactForm(Form):
   jira_name = TextField("JIRA Name",  [validators.Required("Please enter your jira name.")])
-  jira_password = TextField("JIRA Password",  [validators.Required("Please enter your jira password.")])
+  jira_password = PasswordField("JIRA Password",  [validators.Required("Please enter your jira password.")])
   upload = FileField('upload', validators=[validators.Required("Please choose a file."), FileAllowed(['txt'], 'Please upload .txt file')])  
   submit = SubmitField("Start!")
-  select_type = SelectField("Post Type", choices=[('bug','Bugs'),('test_step','Test Steps')])
+  select_type = SelectField("Post Type", choices=[('test_step','Test Steps'),('bug','Bugs')])
 
 
